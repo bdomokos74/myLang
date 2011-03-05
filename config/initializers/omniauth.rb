c1f3a695b6b8dd28544df2f7fd8e022faf257a45
Balint Domokos <bdomokos74@yahoo.com>
2011-03-05 16:50:52 +0100

    Testing omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '21ce26b54c3c87a28bf2', 'e6fff27349e290afb8020917357ba4c7e17d8b84'
+  provider :facebook, '137958536270378', '376cfbf2e0269739376293516078e242'
 end
